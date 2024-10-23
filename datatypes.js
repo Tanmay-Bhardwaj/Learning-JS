9133f164c6684713d2cc39f5b49bdb4cd2bc1a06
Tanmay Bhardwaj <143530146+Tanmay-Bhardwaj@users.noreply.github.com>
2024-10-23 15:58:37 +0000

    p

---
+++ datatypes.js
@@ -0,0 +1,30 @@
+"use strict"; //treat all JS code as newer version
+/*
+alert("hello")
+alert(3+3)
+we are using nodejs,not browser
+*/
+
+//THERE SHOULD BE CODE READBILITY
+
+//JS documentation mdn (not original)
+//tc39.es (ecma script,ORIGINAL)
+
+let name="Tanmay"
+let age= 15
+let isLoggedIn= false
+let state=null
+
+//number => 2 to power 53
+//bigint
+//string=> " "
+//boolean => true/false
+//null => standalone value (empty)
+//undefined => 
+//symbol => unique
+
+//object
+
+console.log(typeof "hello");
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
---
+++ variable.js
@@ -0,0 +1,25 @@
+const accountId=144553
+let accountEmail = "tan@gmail.com"
+var accountPassword ="12345"
+accountCity= "Pune"
+let accountstate;
+
+/**Const cannot be changed  */
+
+//accountId=2 //not allowed
+
+accountEmail="hc@hc.com"
+accountPassword="123123"
+accountCity="Delhi"
+
+console.table([accountEmail,accountId,accountPassword,accountCity,accountstate])
+
+/* difference between let and var
+VAR NOT USED
+let is used in new code
+*/
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
